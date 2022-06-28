7c7f838592b5e53d2aa4ab535f235f74007f0a99
ml2618 <meizhu.liang18@imperial.ac.uk>
2022-06-28 20:01:37 +0100

    new commit

--- src/napari_sim_simulator/_tests/test_widget.py
+++ src/napari_sim_simulator/_tests/test_widget.py
@@ -7,5 +7,5 @@ from napari_sim_simulator import SIMulator
 # capsys is a pytest fixture that captures stdout and stderr output streams
 def test_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
-    simulator_widget = SIMulator().w
+    simulator_widget = SIMulator(viewer).w
     simulator_widget(viewer)
