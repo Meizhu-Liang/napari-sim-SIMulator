0f7c3639a8cd6756d679d1a4894b8e2f639388e0
ml2618 <meizhu.liang18@imperial.ac.uk>
2022-06-28 17:43:12 +0100

    new commit

--- src/napari_sim_simulator/__init__.py
+++ src/napari_sim_simulator/__init__.py
@@ -1,15 +1,12 @@
 __version__ = "0.0.1"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
-from ._writer import write_multiple, write_single_image
 
-__all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+__author__ = "Andrea Bassi and Mark Neil"
+__email__ = "andreabassi@polimi.it"
+
+
+from ._widget import SIMulator
